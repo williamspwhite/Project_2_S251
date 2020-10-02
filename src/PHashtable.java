02d9560b9d9b71eb73d72521024bdfa2d223ede5
williamspwhite <williamspwhite@gmail.com>
2020-10-02 11:07:24 -0400

    Completed PatientQueue and PHashtable

--- src/PHashtable.java
+++ src/PHashtable.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 public class PHashtable {
-    private ArrayList[] table;
+    private ArrayList<Patient>[] table;
     private int t_capacity;
     private int size;
     //set the table size to the first 
     //prime number p >= capacity
     public PHashtable (int capacity) {
         table = new ArrayList[getNextPrime(capacity)];
-        t_capacity = capacity;
+        t_capacity = getNextPrime(capacity);
         size = 0;
 	//TO BE COMPLETED
     }
@@ -15,24 +15,60 @@ public class PHashtable {
     //return the Patient with the given name 
     //or null if the Patient is not in the table
     public Patient get(String name) {
-        System.out.println(name.hashCode() % t_capacity);
+        int hash_n;
+        if ((hash_n = name.hashCode() % t_capacity) < 0) {
+            hash_n = hash_n + t_capacity;
+        }
+        if (table[hash_n] == null) {
+            return  null;
+        }
+        for (Patient patient : table[hash_n]) {
+            if ((patient.name()).compareTo(name) == 0) {
+                return patient;
+            }
+        }
         return null;
 	//TO BE COMPLETED
     }
 
     //put Patient p into the table
     public void put(Patient p) {
-        size++;
+        int hash_p;
+        if ((hash_p = p.name().hashCode() % t_capacity) < 0) {
+            hash_p = hash_p + t_capacity;
+        }
+        if (get(p.name()) == null) {
+            if (table[hash_p] == null) {
+                table[hash_p] = new ArrayList<Patient>();
+            }
+            table[hash_p].add(p);
+            size++;
+            return;
+        }
+
         return;
 	//TO BE COMPLETED
     }
 
-    //remove and returnthe Patient with the given name
+    //remove and return the Patient with the given name
     //from the table
     //return null if Patient doesn't exist
     public Patient remove(String name) {
-        size--;
-        return null;
+        int hash_n;
+        if ((hash_n = name.hashCode() % t_capacity) < 0) {
+            hash_n = hash_n + t_capacity;
+        }
+        Patient return_patient = null;
+        if ((return_patient = get(name)) == null) {
+            return return_patient;
+        } else {
+            table[hash_n].remove(return_patient);
+            size--;
+            if (table[hash_n].size() == 0) {
+                table[hash_n] = null;
+            }
+            return return_patient;
+        }
 	//TO BE COMPLETED
     }	    
 
